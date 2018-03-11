22c6caa2ce2f4d1abb028fdec2aa712be83d82ac
minhthuy30197 <minhthuy30197@gmail.com>
2018-03-11 09:32:15 +0700

    delete + change state + add tasks

--- models/task.js
+++ models/task.js
@@ -1,12 +1,26 @@
-let Task = function(name) {
+let Task = function(name, id) {
   let _name = name;
+  let _id = id;
+  let done = false;
 
   this.getName = function () {
     return _name;
   }
 
+  this.getID = function () {
+    return _id;
+  }
+
+  this.getDone = function () {
+    return done;
+  }
+
   this.setName = function (name) {
     _name = name;
+  }
+
+  this.setDone = function (state) {
+    done = state;
   }
 };
 
