ae9621658ffd15fe296fb6a03b9080187b1444c2
vuchucanh <lananh.269@gmail.com>
2018-03-19 17:05:33 +0700

    Edit 1903

--- MainPage.js
+++ MainPage.js
@@ -30,7 +30,8 @@ app.get('/done',function (req,res) {
 
 app.get('/pomo',function (req,res) {
   let id = req.param('id')
-  res.status(200).send(id)
+  pos = pomodoroClock ()
+  res.status(302).redirect('/')
 })
 
 app.get('/fix',function (req,res) {
@@ -44,6 +45,7 @@ app.get('/reset',function (req,res) {
   tasks[pos].setDone(false)
   res.status(302).redirect('/')
 })
+
 
 app.post('/insert', urlencodedParser,function (req,res) {
   console.log(req.body.newtask)
@@ -80,4 +82,78 @@ function findTask (id) {
     i++;
   }
 }
-
+function pomodoroClock () {
+var pomodoro = {
+  started : false,
+  minutes : 0,
+  seconds : 0,
+  fillerHeight : 0,
+  fillerIncrement : 0,
+  interval : null,
+  minutesDom : null,
+  secondsDom : null,
+  fillerDom : null,
+  init : function(){
+    var self = this;
+    this.minutesDom = document.querySelector('#minutes');
+    this.secondsDom = document.querySelector('#seconds');
+    this.fillerDom = document.querySelector('#filler');
+    this.interval = setInterval(function(){
+      self.intervalCallback.apply(self);
+    }, 1000);
+    document.querySelector('#work').onclick = function(){
+      self.startWork.apply(self);
+    };
+    document.querySelector('#stop').onclick = function(){
+      self.stopTimer.apply(self);
+    };
+  },
+  resetVariables : function(mins, secs, started){
+    this.minutes = mins;
+    this.seconds = secs;
+    this.started = started;
+    this.fillerIncrement = 200/(this.minutes*60);
+    this.fillerHeight = 0;
+  },
+  startWork: function() {
+    this.resetVariables(25, 0, true);
+  },
+  stopTimer : function(){
+    this.resetVariables(25, 0, false);
+    this.updateDom();
+  },
+  toDoubleDigit : function(num){
+    if(num < 10) {
+      return "0" + parseInt(num, 10);
+    }
+    return num;
+  },
+  updateDom : function(){
+    this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+    this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+    this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+    this.fillerDom.style.height = this.fillerHeight + 'px';
+  },
+  intervalCallback : function(){
+    if(!this.started) return false;
+    if(this.seconds == 0) {
+      if(this.minutes == 0) {
+        this.timerComplete();
+        return;
+      }
+      this.seconds = 59;
+      this.minutes--;
+    } else {
+      this.seconds--;
+    }
+    this.updateDom();
+  },
+  timerComplete : function(){
+    this.started = false;
+    this.fillerHeight = 0;
+  }
+};
+window.onload = function(){
+  pomodoro.init();
+};
+}
