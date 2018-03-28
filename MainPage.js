5870e9c3b2f0c1a52c0d486222233cb612aa32eb
minhthuy30197 <minhthuy30197@gmail.com>
2018-03-28 16:15:06 +0700

    replacing with Ajax

--- MainPage.js
+++ MainPage.js
@@ -2,34 +2,32 @@ require('dotenv').config()
 
 let express = require('express')
 let Task = require('./models/task')
-var bodyParser = require('body-parser')
-//var urlencodedParser = bodyParser.urlencoded({ extended: false})
+let bodyParser = require('body-parser')
 
 let app = new express()
-//app.use(bodyParser.json())
-var json_body_parser = bodyParser.json();
-var urlencoded_body_parser = bodyParser.urlencoded({ extended: true });
-app.use(json_body_parser);
-app.use(urlencoded_body_parser);
-app.use(express.static(__dirname+'/public'))
+let json_body_parser = bodyParser.json()
+let urlencoded_body_parser = bodyParser.urlencoded({extended: true})
+app.use(json_body_parser)
+app.use(urlencoded_body_parser)
+app.use(express.static(__dirname + '/public'))
 
 app.set('view engine', 'ejs')
 app.set('port', process.env.WEB_PORT)
 
 let tasks = getTasksFromDatabase()
 
-app.get('/delete', function (req, res) {
-  let id = req.param('id')
-  pos = findTask(id)
+app.post('/delete', function (req, res) {
+  let obj = req.body
+  pos = findTask(obj['id'])
   tasks.splice(pos, 1)
-  res.status(302).redirect('/')
+  res.status(200).send({result: 'success'})
 })
 
-app.get('/done', function (req, res) {
-  let id = req.param('id')
-  pos = findTask(id)
+app.post('/done', function (req, res) {
+  let obj = req.body
+  pos = findTask(obj['id'])
   tasks[pos].setDone(true)
-  res.status(302).redirect('/')
+  res.status(200).send({result: 'success', name: tasks[pos].getName()})
 })
 
 app.get('/pomo', function (req, res) {
@@ -42,47 +40,57 @@ app.get('/fix', function (req, res) {
   res.status(200).send(id)
 })
 
-app.get('/reset', function (req, res) {
-  let id = req.param('id')
-  pos = findTask(id)
+app.post('/reset', function (req, res) {
+  let obj = req.body
+  pos = findTask(obj['id'])
   tasks[pos].setDone(false)
-  console.log(tasks[pos])
-  res.status(302).redirect('/')
+  res.status(200).send({result: 'success'})
 })
 
-app.post('/insert', urlencoded_body_parser,function (req,res) {
-  console.log(req.body.newtask)
-  let newtask = new Task(req.body.newtask, tasks.length+1)
-  console.log(newtask.getID())
+app.post('/insert', function (req, res) {
+  let obj = req.body
+  let newtask = new Task(obj['name'], tasks.length + 1)
   tasks.push(newtask)
-  res.status(302).redirect('/')
+  res.status(200).send({result: 'success', id: newtask.getID()})
+})
+
+app.post('/changetask', function (req, res) {
+  let obj = req.body
+  console.log(obj['name'])
+  let task = tasks[findTask(obj['id'])]
+  task.setName(obj['name'])
+  res.status(200).send({result: 'success'})
+})
+
+app.get('/task', function (req, res) {
+  let id = req.param('id')
+  let task = tasks[findTask(id)]
+  let obj = {'name': task.getName(), 'id': task.getID()}
+  res.status(200).send(JSON.stringify(obj))
+})
+
+app.get('/gettask', function (req, res) {
+  let arr = []
+  let objarr = {tasks: arr}
+  let i = 0
+  while (i < tasks.length) {
+    arr[i] = {'name' : tasks[i].getName(), 'id' : tasks[i].getID(), 'done' : tasks[i].getDone()}
+    i++
+  }
+  console.log(objarr)
+  res.status(200).send(JSON.stringify(objarr))
 })
 
 app.get('/', function (req, res) {
   let view = 'MainPage'
   let params = {
-    tasks: tasks
+    tasks: tasks,
   }
   res.status(200).render(view, params)
 })
 
-app.get('/task',function (req,res) {
-  let id = req.param('id')
-  let task = tasks[findTask(id)]
-  var obj = { "name": task.getName(), "id": task.getID() }
-  res.status(200).send(JSON.stringify(obj))
-})
-
 app.listen(app.get('port'), function () {
   console.log('Server running at http://localhost:' + app.get('port'))
-})
-
-app.post('/changetask',function (req, res) {
-  let obj = req.body
-  console.log(obj['name'])
-  let task = tasks[findTask(obj['id'])]
-  task.setName(obj['name'])
-  res.status(200).send({result:"success"})
 })
 
 function getTasksFromDatabase () {
@@ -91,7 +99,6 @@ function getTasksFromDatabase () {
   let task03 = new Task('implement feature #5', 3)
   return [task01, task02, task03]
 }
-
 
 function findTask (id) {
   i = 0
